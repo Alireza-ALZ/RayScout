0450db95e6e0d03e3dab12137b5f5bcd2b55386f
Alireza Alizadeh <A84Alizadeh@Gmail.com>
2026-07-10 02:13:58 +0330

    fix: Make one of configs invalid to test probe engine

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ProbeEngine = require("./src/probe/probeEngine");
 const XrayConfigBuilder = require("./src/xray/XrayConfigBuilder");
 
 const allConfigs = [
-  "vless://UUID@domain.com:443?type=ws&security=tls&path=%2Fws&sni=domain.com#My%20VLESS",
+  "vless://UUID@domainnnnnnnnnnnnnnnn.com:444?type=ws&security=tls&path=%2Fws&sni=domain.com#My%20VLESS",
   "vmess://eyJhZGQiOiJleGFtcGxlLmNvbSIsInBvcnQiOiI0NDMiLCJpZCI6ImFiYzEtMTIzNCIsImFpZCI6IjAiLCJuZXQiOiJ3cyIsInBhdGgiOiIvd3MiLCJ0bHMiOiJ0bHMiLCJwcyI6Ik15IFZNRVNzIn0=#My VMess Server",
 ];
 
